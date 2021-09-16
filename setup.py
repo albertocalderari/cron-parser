9fa71894b1c525338427c94eb6d9630a6b4e779f
Alberto Calderari <aberto.calderari@gmail.com>
2021-09-16 08:07:19 +0100

    use requirements from requirements file

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
+with open("./requirements.txt") as f:
+    requirements = [l for l in f.readlines() if not l.startswith("#")]
+
 setup(
     name='cronparser',
     version='0.1.0',
     packages=find_packages(include=["cronparser", "cronparser.*"], exclude=["tests"]),
-    install_requires=["marshmallow-dataclass==8.5.3"],
+    install_requires=[requirements],
     entry_points={
         'console_scripts': ['cronparser=cronparser.main:main']
     }
